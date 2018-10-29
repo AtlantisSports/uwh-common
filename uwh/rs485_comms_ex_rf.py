cb291bb1de4ed0f7b1d57e2d1f123723b24c3398
Tristan Debrunner <tpwd@me.com>
2018-10-29 12:29:48 -0600

    UNTESTED: added rs485 comms examples

---
+++ uwh/rs485_comms_ex_rf.py
@@ -0,0 +1,28 @@
+import serial
+import sys
+from time import sleep
+
+
+if len(sys.argv) != 2:
+    print("Please provide the tty device as the only argument!")
+    exit()
+
+device = sys.argv[1]
+
+# Setup the serial device, defaults to 8N0. second argument is baud rate.
+# See https://pyserial.readthedocs.io/en/latest/pyserial_api.html for
+# description of timeout modes
+ser = serial.Serial(device, 115200, timeout=0)
+
+while True:
+    recv = ser.read()
+    if len(recv) != 0:
+        print("Recieved: {}".format(recv))
+        break
+    sleep(0.1)
+
+sleep(0.5)
+
+ser.write(b't')
+print("Sent: t")
+ser.close()
