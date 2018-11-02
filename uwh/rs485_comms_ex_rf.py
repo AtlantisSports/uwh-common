c2e19eb0acacfb21349742e8f0e3befae98c4c8b
Tristan Debrunner <tpwd@me.com>
2018-11-02 11:22:54 -0600

    Added multi-byte sends

--- uwh/rs485_comms_ex_rf.py
+++ uwh/rs485_comms_ex_rf.py
@@ -11,7 +11,8 @@ device = sys.argv[1]
 
 # Setup the serial device, defaults to 8N0. second argument is baud rate.
 # See https://pyserial.readthedocs.io/en/latest/pyserial_api.html for
-# description of timeout modes
+# description of timeout modes. timeout=0 will cause it to immediately
+# return the contents of the input buffer when read() is called.
 ser = serial.Serial(device, 115200, timeout=0)
 
 while True:
@@ -25,4 +26,18 @@ sleep(0.5)
 
 ser.write(b't')
 print("Sent: t")
+try:
+    string = b""
+    while True:
+        string += ser.read()
+        if len(string) > 0 and string[-1] == b'\n'[0]:
+            print("Recieved: {}".format(string))
+            break
+        sleep(0.1)
+except KeyboardInterrupt:
+    print("Current String: {}".format(string))
+
+ser.write(b'world\n')
+print("Sent: 'world'")
+
 ser.close()
--- uwh/rs485_comms_ex_tf.py
+++ uwh/rs485_comms_ex_tf.py
@@ -24,4 +24,18 @@ while True:
         break
     sleep(0.1)
 
+ser.write(b'hello\n')
+print("Sent: 'hello'")
+
+try:
+    string = b""
+    while True:
+        string += ser.read()
+        if len(string) > 0 and string[-1] == b'\n'[0]:
+            print("Recieved: {}".format(string))
+            break
+        sleep(0.1)
+except KeyboardInterrupt:
+    print("Current String: {}".format(string))
+
 ser.close()
